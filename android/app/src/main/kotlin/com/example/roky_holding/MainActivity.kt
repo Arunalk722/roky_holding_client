af324ee6f130f5340fa45be55ce0e74e16f2f54e
Aruna Shantha-Laptop <arunalk722@hotmail.com>
2025-07-02 20:23:55 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/roky_holding/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.roky_holding
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/env/api_info.dart
@@ -0,0 +1,37 @@
+import 'package:url_launcher/url_launcher.dart';
+
+class APIHost {
+    final String apiURL = 'http://localhost:8002/RN/public/apis/controllers';
+    // final String apiURL = 'https://apps.api.rokyholdings.com/RN/public/apis/controllers';
+  final String appVersion = '3.0.10';
+}
+class APIInfo{
+  static String _apiVersion='';
+  static void setAPI(String val){
+    _apiVersion=val;
+  }
+  static String getAPI(){
+    return _apiVersion;
+  }
+}
+class APIToken {
+  String? _token;
+  APIToken._privateConstructor();
+  static final APIToken _instance = APIToken._privateConstructor();
+
+  factory APIToken() {
+    return _instance;
+  }
+  //token
+  set token(String? value) => _token = value;
+  String? get token => _token;
+}
+Future<void> openWebPage() async {
+  final Uri uri = Uri.parse('https://apps.rokyholdings.com/app_clear.html');
+  if (await canLaunchUrl(uri)) {
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  } else {
+  }
+}
+
+
